fd680fda5c5e568ad36561bc2fb8d64541959d08
chuantao.ou@han-networks.com <chuantao.ou@han-networks.com>
2020-03-17 17:41:46 +0800

    connect to database and paging query

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,11 +2,13 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Created by OY on 2019/11/8.
  */
 @SpringBootApplication
+@EnableJpaRepositories
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
---
+++ src/main/java/com/web/Entity/PageUtil.java
@@ -0,0 +1,32 @@
+package com.web.Entity;
+
+/**
+ * @version 1.0
+ * @ProjectName: learning
+ * @Package: com.web.Entity
+ * @ClassName: PageUtil
+ * @Description: default
+ * @Author: Administrator
+ * @CreateDate: 2020/3/17 17:37
+ */
+public class PageUtil {
+
+    private int pageNumber;
+    private int pageSize;
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+}
